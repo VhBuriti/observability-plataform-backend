1d74d956950b46d0b9fec76ae2255b7b7923a35e
VhBuriti <victorhugoburit@hotmail.com>
2026-03-18 12:14:51 -0300

    feat: adding basic helper function and fixing nodemon

--- routes/heatmap.js
+++ routes/heatmap.js
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import fs, { write } from 'fs';
+
+async function writeFile(text) {
+    try {
+        await fs.appendFileSync("data/heatmap.json", JSON.stringify(text));
+
+        return 'File written sucessefully'
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 
 const router = Router();
 
-router.post("/:siteName", (req, res) => {
-    const { siteName } = req.params;
+router.post("/", (req, res) => {
+    const { page, events } = req.body;
 
+
+    writeFile({ page, events })
     
 })
 
