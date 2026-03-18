1fc585d55756f136b9f876cbb1622fb0ce740ca8
VhBuriti <victorhugoburit@hotmail.com>
2026-03-18 11:24:52 -0300

    chore: adding heatmap basic routing

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 import express from "express";
 import cors from "cors";
-import fs from 'fs';
+import heatmapRoutes from "./routes/heatmap.js";
+
 
 const app = express();
 const port = 8080;
-const router = express.Router();
 
-app.use(cors()); // enable CORS
+app.use(cors());
 app.use(express.json());
-app.use(router);
 
-router.post("/api/event", (req, res) => {
-  console.log(req.body);
+app.use("/heatmap", heatmapRoutes);
 
-  fs.writeFile("./data/events.json", JSON.stringify(req.body), (err) => {
-    if (err) {
-      console.error("Error writing to file:", err);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-  });
 
-  res.status(200).json({ message: "Event received successfully" });
-});
 
 app.listen(port, () => {
   console.log(`API running at http://localhost:${port}`);
---
+++ routes/heatmap.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.post("/:siteName", (req, res) => {
+    const { siteName } = req.params;
+
+    
+})
+
+
+export default router;
